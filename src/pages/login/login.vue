9bcf42da4af2f01ac3b5844f7386ec219eb63c3f
xuexingwei <xuexingwei@bus365.com>
2018-03-08 11:14:36 +0800

    多文件应用模板

---
+++ src/pages/login/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="login">
+      <img src="@/assets/logo.png">
+      <h1>this is login</h1>
+      <a href="index.html">login.html</a>
+    </div>
+</template>
+
+
+<style>
+#login {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
